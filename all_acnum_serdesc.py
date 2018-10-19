1f7953a31cdbbac7502c8b14f22d03bff69e08ec
Yoonsun You <35348067+youyoons@users.noreply.github.com>
2018-10-19 13:39:37 -0400

    Fix bugs and remove duplicates in acnum, seriesdesc in all_acnum_serdesc.py

--- all_acnum_serdesc.py
+++ all_acnum_serdesc.py
@@ -1,15 +1,8 @@
 import os
-import sys
-import shutil
-import settings
-import utils
-import cv2
-import SimpleITK
 import numpy as np
 import pydicom
-import math
-import pandas
 
+#Assumes that path is one level above the Study directory level
 def get_acNum_serDesc(path):
     #Assumes path gives file path to batch
     studies = os.listdir(path)
@@ -18,22 +11,31 @@ def get_acNum_serDesc(path):
     series_description = []
     
     for study in studies:
+        #print("On Study: ", study)
         series_path = os.path.join(path,study)
         
-        series = os.listdir(series_path)[0] #Get the first dicom file in a series
+        series = os.listdir(series_path)
+                
+        #Get the first dicom file in a series
+        first_file = series[0]
         
-        scan = pydicom.read_file(series)
+        if ".dcm" in first_file:
+            #print(first_file)
+            scan = pydicom.read_file(os.path.join(series_path,first_file))
+            
+            if scan.AccessionNumber not in accession_number:
+                accession_number.append(scan.AccessionNumber)
+            if scan.SeriesDescription not in series_description:
+                accession_number.append(scan.SeriesDescription)
         
-        accession_number.append(scan.AccessionNumber)
-        series_description.append(scan.SeriesDescription)
     
-    return accession_number
+    return accession_number, series_description
     
 
 if __name__=="__main__":
     path = "/media/ubuntu/cryptscratch/cspine_data/batch1/Final/"
     
-    acNum,serDesc = get_acNum_seriesDesc(path)
+    acNum,serDesc = get_acNum_serDesc(path)
     
     print("Accession Numbers: ", acNum)
     print("Series Descriptions: ", serDesc)
