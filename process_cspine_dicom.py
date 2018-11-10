f6a8d26bb3fd02046a0dbcae1fab01bcd42320d7
Yoonsun You <yoonsunryan.you@mail.utoronto.ca>
2018-11-10 14:22:10 -0500

    Updating for organization

--- process_cspine_dicom.py
+++ process_cspine_dicom.py
@@ -25,7 +25,7 @@ def inspect_scan(scan_path):
     print(scan.dir())
     print(scan)
 
-def process_study(scans,file_names,base_dir,patient,exclusions,log_path):
+def process_study(scans,file_names,patient,exclusions,log_path,path_raw,path_proc):
 
     all_series = {} #stores all the series (studies done on a single patient)
 
@@ -49,13 +49,16 @@ def process_study(scans,file_names,base_dir,patient,exclusions,log_path):
         else:
             all_series[series].append([instance,file_names[index],series_desc])
 
-    #create directories for different series0=bcdfoprst
-    study_path_raw = os.path.join("/home/youy/Documents/Spine/RawData_y",accessnum)
-    study_path_proc = os.path.join("/home/youy/Documents/Spine/ProcessedData_y",accessnum)
-
     #Remove All Files/Directories Existing within RawData and ProcessedData
     #os.rmdir("/home/youy/Documents/Spine/RawData_y")
     #os.rmdir("/home/youy/Documents/Spine/ProcessedData_y")
+
+    #create raw and processed directory if they don't exist
+    [os.mkdir(path) for path in [path_raw,path_proc] if not os.path.exists(path)]
+
+    #create directories for different series
+    study_path_raw = os.path.join(path_raw,accessnum)
+    study_path_proc = os.path.join(path_proc,accessnum)
 
     [os.mkdir(path) for path in [study_path_raw,study_path_proc] if not os.path.exists(path)]
 
@@ -65,104 +68,101 @@ def process_study(scans,file_names,base_dir,patient,exclusions,log_path):
     valid_series_desc = ["C SPINE BONE SAG 1.2", "C-SPINE BONE SAG 1.2", "C-SPINE SAG", "C-SPINE SPINE BONE SAG",
                          "CSPINE SAG", "SAG C-SPINE BONE"]
 
-    for series in all_series:
-        if (len(all_series[series])) > 1 and any(all_series[series][0][2] in s for s in valid_series_desc):
-            if not os.path.exists(os.path.join(study_path_raw,series)):
-                os.mkdir(os.path.join(study_path_raw,series))
-
-            if not os.path.exists(os.path.join(study_path_proc,series)):
-                os.mkdir(os.path.join(study_path_proc,series))
-
-
-    #move raw files into directories
-    for series in all_series:
-        instances = all_series[series]
-
-        if (len(all_series[series])) > 1 and any(all_series[series][0][2] in s for s in valid_series_desc):
-            for scan in all_series[series]:
-                src = scan[1] #DICOM file location
-                dst = os.path.join(study_path_raw,series,str(scan[0])+".dcm")
-
-                if not os.path.exists(dst):
-                    #shutil.move(src,dst)
-                    copyfile(src,dst)
-                else:
-                    dst = os.path.join(study_path_raw,series,str(scan[0])+".dcm")
-
     #for linking log entries
     log_entries = []
 
-    #dicom to dicom and save
     for series in all_series:
+        instances = all_series[series]
+
+        #Create raw and processed series directories if they do not exist
         if (len(all_series[series])) > 1 and any(all_series[series][0][2] in s for s in valid_series_desc):
-            series_dir = os.path.join(study_path_raw,series)
-            series_files = [os.path.join(series_dir,file_name) for file_name in os.listdir(series_dir)]
+            #In terms of processed as we remove raw studies right after it is done
+            print(os.path.join(study_path_proc,series))
+            
+            if not os.path.exists(os.path.join(study_path_proc,series)):
+                os.mkdir(os.path.join(study_path_raw,series))
+                os.mkdir(os.path.join(study_path_proc,series))
 
-            if len(series_files)<2:
-                print("Series {} has less than two files".format(series))
-                continue
+                #Copy over files into raw path
+                for scan in all_series[series]:
+                    src = scan[1] #DICOM file location
+                    dst = os.path.join(study_path_raw,series,str(scan[0])+".dcm")
 
-            series_scans = [pydicom.read_file(file_name) for file_name in series_files]
-            series_scans.sort(key = lambda x: float(x.InstanceNumber))
+                    if not os.path.exists(dst):
+                        #shutil.move(src,dst)
+                        copyfile(src,dst)
+                    else:
+                        dst = os.path.join(study_path_raw,series,str(scan[0])+".dcm")
 
-            try:
-                slice_thickness = np.abs(series_scans[0].ImagePositionPatient[2] - series_scans[1].ImagePositionPatient[2])
-            except:
+                #dicom to dicom and save
+
+                series_dir = os.path.join(study_path_raw,series)
+                series_files = [os.path.join(series_dir,file_name) for file_name in os.listdir(series_dir)]
+
+                if len(series_files)<2:
+                    print("Series {} has less than two files".format(series))
+                    continue
+
+                series_scans = [pydicom.read_file(file_name) for file_name in series_files]
+                series_scans.sort(key = lambda x: float(x.InstanceNumber))
+
                 try:
-                    slice_thickness = np.abs(series_scans[0].SliceLocation - series_scans[1].SliceLocation)
+                    slice_thickness = np.abs(series_scans[0].ImagePositionPatient[2] - series_scans[1].ImagePositionPatient[2])
                 except:
-                    slice_thickness = np.abs(0)
+                    try:
+                        slice_thickness = np.abs(series_scans[0].SliceLocation - series_scans[1].SliceLocation)
+                    except:
+                        slice_thickness = np.abs(0)
 
-            for s in series_scans:
-                s.SliceThickness = slice_thickness
+                for s in series_scans:
+                    s.SliceThickness = slice_thickness
 
-            instance_nums = [scan.InstanceNumber for scan in series_scans]
+                instance_nums = [scan.InstanceNumber for scan in series_scans]
 
-            for index,scan in enumerate(series_scans):
-                file_name = "scan_%s_%s" % (str(instance_nums[index]).rjust(4, '0'),".dcm")
-                log_ids = [study,series,file_name]
+                for index,scan in enumerate(series_scans):
+                    file_name = "scan_%s_%s" % (str(instance_nums[index]).rjust(4, '0'),".dcm")
+                    log_ids = [study,series,file_name]
 
-                #saving path of anonymized one to proc directory
-                save_path = os.path.join(study_path_proc,series,file_name)
+                    #saving path of anonymized one to proc directory
+                    save_path = os.path.join(study_path_proc,series,file_name)
 
-                all_attributes = scan.dir()
-                log_attr = []
-                #print(all_attributes)
-                for attr in all_attributes:
-                    if attr in exclusions:
-                        tag = scan.data_element(attr).tag
-                        value = scan[tag].value
-                        log_attr.append(value)
-                        del scan[tag]
+                    all_attributes = scan.dir()
+                    log_attr = []
+                    #print(all_attributes)
+                
+                    for attr in all_attributes:
+                        if attr in exclusions:
+                            tag = scan.data_element(attr).tag
+                            value = scan[tag].value
+                            log_attr.append(value)
+                            del scan[tag]
 
-                print("Saving processed study to {}".format(save_path))
+                    print("Saving processed study to {}".format(save_path))
 
-                #saving to processed location
-                scan.save_as(save_path)
+                    #saving to processed location
+                    scan.save_as(save_path)
 
-                #adding to log_entries, which will be used for the linking log
-                log_entries.append(log_ids+log_attr)
+                    #adding to log_entries, which will be used for the linking log
+                    log_entries.append(log_ids+log_attr)
 
-            print("Series {} complete".format(series))
+                print("Series {} complete".format(series))
 
 
 
-    directories = os.listdir("/home/youy/Documents/Spine/RawData_y/")
+    directories = os.listdir("/home/youy/Documents/Spine/ProcessedData_y/")
     for i in directories:
-        if len(os.listdir(os.path.join("/home/youy/Documents/Spine/RawData_y/",i))) == 0:
-            os.rmdir(os.path.join("/home/youy/Documents/Spine/RawData_y/",i))
+        if len(os.listdir(os.path.join("/home/youy/Documents/Spine/ProcessedData_y/",i))) == 0:
             os.rmdir(os.path.join("/home/youy/Documents/Spine/ProcessedData_y/", i))
 
+    #Remove raw directory path after each study
+    shutil.rmtree(study_path_raw)    
 
     return log_entries
 
 
-def process_scans(data_dir="/media/ubuntu/cryptscratch/scratch/youy/Data/Spine/"):
+def process_scans(raw_data_dir,path_raw,path_proc,log_path):
     #Must be in alphabetical order
     dicom_exclusions = ['AccessionNumber','AdditionalPatientHistory','InstitutionName','NameOfPhysiciansReadingStudy','OtherPatientIDs','OtherPatientIDsSequence','PatientBirthDate','PatientID','PatientName','ReferringPhysicianName']
-
-    raw_data_dir = "/home/youy/Documents/Spine/batch_test/"
-    log_path = "/home/youy/Documents/Spine/linking_log.csv"
 
     patients = os.listdir(raw_data_dir)
     print(patients)
@@ -181,7 +181,7 @@ def process_scans(data_dir="/media/ubuntu/cryptscratch/scratch/youy/Data/Spine/"
             file_names = [os.path.join(patient_path,s) for s in dicom_files]
             #print(slices)
 
-            log_line = process_study(slices,file_names,data_dir,patient,dicom_exclusions,log_path)
+            log_line = process_study(slices,file_names,patient,dicom_exclusions,log_path,path_raw,path_proc)
             #print(log_line)
             log_entries.extend(log_line)
             print('-------------------------------------------')
@@ -195,5 +195,11 @@ def process_scans(data_dir="/media/ubuntu/cryptscratch/scratch/youy/Data/Spine/"
     save_log(log_path,log_entries,dicom_exclusions)
 
 if __name__=="__main__":
-    process_scans()
+
+    raw_data_dir = "/home/youy/Documents/Spine/batch1/"   
+    path_raw = "/home/youy/Documents/Spine/RawData_y"
+    path_proc = "/home/youy/Documents/Spine/ProcessedData_y"
+    log_path = "/home/youy/Documents/Spine/linking_log.csv"
+
+    process_scans(raw_data_dir,path_raw,path_proc,log_path)
 
