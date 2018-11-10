492dde851e33d810e3bbfc0a722af8961aaaecf8
Yoonsun You <yoonsunryan.you@mail.utoronto.ca>
2018-11-10 15:16:33 -0500

    Updating file list and copying scripts

--- Copy_Scans/get_paths.py
+++ Copy_Scans/get_paths.py
@@ -2,19 +2,18 @@ import openpyxl
 import os
 import numpy as np
 
-book = openpyxl.load_workbook('CS_accession_number_paths_2018_1025.xlsx')
+#Get spreadsheet that contains paths to CSpine Accession Numbers
+book = openpyxl.load_workbook('CS_accession_number_paths.xlsx')
 sheet = book.active
 row_count = sheet.max_row
-#print(row_count)
 
-paths = []
+frac_log = open("path_frac.log","w")
 
 for row in range(2,row_count+1):
     if sheet.cell(row,3).value == 1 or sheet.cell(row,4).value == 1:
         path_frac = sheet.cell(row,2).value
 
         if path_frac != None:
-            paths.append(path_frac)
-            print(path_frac)
+            frac_log.write(path_frac + "\n")
 
-#print(paths[1:20])
+frac_log.close()
