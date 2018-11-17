5b9a088e1e6644371bd2bf9fb6c27e47e1689af6
Yoonsun You <yoonsunryan.you@mail.utoronto.ca>
2018-11-17 15:21:19 -0500

    Modifying to allow choice between fracture list and non-fracture list

--- Copy_Scans/get_paths.py
+++ Copy_Scans/get_paths.py
@@ -2,6 +2,10 @@ import openpyxl
 import os
 import numpy as np
 
+#True if we want to get paths with only fractures
+#False if we want to get paths without any fractures
+fracture = True
+
 #Get spreadsheet that contains paths to CSpine Accession Numbers
 book = openpyxl.load_workbook('CS_accession_number_paths.xlsx')
 sheet = book.active
@@ -9,10 +13,20 @@ row_count = sheet.max_row
 
 frac_log = open("path_frac.log","w")
 
-for row in range(2,row_count+1):
-    if sheet.cell(row,3).value == 1 or sheet.cell(row,4).value == 1:
-        path_frac = sheet.cell(row,2).value
 
+for row in range(2,row_count+1):
+    #Fractures
+    if fracture:
+        if sheet.cell(row,3).value == 1 or sheet.cell(row,4).value == 1:
+            path_frac = sheet.cell(row,2).value
+        
+            if path_frac != None:   
+                frac_log.write(path_frac + "\n")
+    #No Fractures
+    else:
+        if sheet.cell(row,3).value != 1 and sheet.cell(row,4).value != 1:
+        path_frac = sheet.cell(row,2).value
+        
         if path_frac != None:
             frac_log.write(path_frac + "\n")
 
