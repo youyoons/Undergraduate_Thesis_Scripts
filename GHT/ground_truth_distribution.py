5677f6b919bd2ee28fd2991c7b657c86c4b9a68c
Yoonsun You <yoonsunryan.you@mail.utoronto.ca>
2019-01-26 21:53:10 -0500

    Adding a script to plot the prior distribution of ground truth points

---
+++ GHT/ground_truth_distribution.py
@@ -0,0 +1,51 @@
+import openpyxl
+import os
+import matplotlib.pyplot as plt
+
+os.chdir("C:\\Users\\yoons\\Documents\\4th Year Semester 2\\ESC499 - Thesis\\Undergraduate_Thesis_Scripts\\GHT")
+
+book = openpyxl.load_workbook("../GHT/ground_truth_detection_pts_all.xlsx")
+sheet = book.active
+row_count = sheet.max_row
+
+ground_truth = {}
+
+x_total = 0
+y_total = 0
+z_total = 0
+
+for i in range(3,row_count): #divided by 3 as a test 
+    
+    ac_num_loc = sheet.cell(row = i,column = 1)
+    ac_num = str(ac_num_loc.value)
+    
+    x = sheet.cell(row = i, column = 2).value
+    y = sheet.cell(row = i, column = 3).value
+    z = sheet.cell(row = i, column = 4).value
+
+    
+    if (x != None) and (y != None) and (z != None):
+        ground_truth[ac_num] = [x,y,z]
+    
+        x_total = x_total + x
+        y_total = y_total + y
+        z_total = z_total + z
+        
+    
+print(ground_truth)
+    
+    
+#Plot non-maximal suppression points
+nms_x_pts = [] 
+nms_y_pts = []
+
+for key in ground_truth.keys():
+    nms_x_pts.append(ground_truth[key][0])
+    nms_y_pts.append(ground_truth[key][1])  
+
+
+plt.scatter(nms_y_pts,nms_x_pts, marker='o', color='g')
+
+plt.savefig("ground_truth_distribution.png")
+
+print(x_total,y_total,z_total)
