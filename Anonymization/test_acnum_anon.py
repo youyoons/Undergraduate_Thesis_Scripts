e0ed4825e0a38726fb0d1d11a0ca6e52472ef1c1
Yoonsun You <yoonsunryan.you@mail.utoronto.ca>
2019-02-04 00:41:06 -0500

    Adding script that anonymized the accession number and creates a hash table spreadsheet to keep track of the changes

---
+++ Anonymization/test_acnum_anon.py
@@ -0,0 +1,59 @@
+import openpyxl
+import os
+
+#os.chdir("C:\\Users\\yoons\\Documents\\ESC499\\Undergraduate_Thesis_Scripts\\Anonymization")
+
+ac_num = '4214124324'
+filepath = "ac_hash.xlsx"
+
+#We should first check if the accession number exists in the hash table before adding it
+if os.path.isfile("ac_hash.xlsx"):
+    exists = False
+    
+    wb = openpyxl.load_workbook(filepath)
+    sheet = wb.active
+    
+    for i in range(1,len(sheet['A'])+1):
+        if sheet.cell(row = i, column = 1).value == ac_num:
+            exists = True
+            new_ac_num = sheet.cell(row = i, column = 2).value
+            break
+            
+    if not exists: #enter value into table if it does not exist
+        row_count = sheet.max_row
+        
+        sheet.cell(row = row_count + 1, column = 1, value = str(ac_num))
+        sheet.cell(row = row_count + 1, column = 2, value = str(row_count).zfill(7))
+        
+        wb.save(filepath)
+        
+        new_ac_num = str(row_count).zfill(7)
+    
+else: #Create workbook and add the mapping
+    #First, save spreadsheet
+    wb = openpyxl.Workbook()
+    wb.save(filepath)
+    
+    #Put in hashed value
+    wb = openpyxl.load_workbook(filepath)
+    sheet = wb.active
+    
+    
+    #Insert first row of headings
+    sheet.cell(row = 1, column = 1, value = "Original AC Num")
+    sheet.cell(row = 1, column = 2, value = 'New AC Num')
+    
+    #Insert second row, which contains data
+    row_count = sheet.max_row
+    
+    sheet.cell(row = row_count + 1, column = 1, value = str(ac_num))
+    sheet.cell(row = row_count + 1, column = 2, value = str(row_count).zfill(7))
+    
+    
+    print(row_count)
+    wb.save(filepath)
+    
+    new_ac_num = str(row_count).zfill(7)
+    
+
+print(new_ac_num)
